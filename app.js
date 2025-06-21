ed0d4454c1a92e99423a8270fc89cc5574a14d76
rcd-env <donerakesh001@gmail.com>
2025-06-21 17:43:02 +0530

    fix apikey issue

--- app.js
+++ app.js
@@ -60,6 +60,7 @@ app.use((req, res, next) => {
   res.locals.success = req.flash("success");
   res.locals.error = req.flash("error");
   res.locals.currUser = req.user;
+  res.locals.GOOGLE_MAP_API_KEY = process.env.GOOGLE_MAP_API_KEY;
   next();
 });
 
